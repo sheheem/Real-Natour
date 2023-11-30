9aa809b648297d0d38a0b145cf8f36e4aee3ccc3
sheheem <sheheemzainudeen@gmail.com>
2023-11-30 15:18:40 +0530

    global error handling

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
 const tourRoute = require('./routes/tourRoutes');
+const AppError = require('./utlils/app-error');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
-
-app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to express', app: 'Natours' });
 });
 
 app.use('/api/v1/tours', tourRoute);
+
+app.all('*', (req, res, next) => {
+  next(
+    new AppError(`Can't find this url ${req.originalUrl} on this server`, 404),
+  );
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
