da3bf7a9550c033df497479bb53dbf304d49601a
sheheem <sheheemzainudeen@gmail.com>
2023-11-30 17:13:37 +0530

    production environment added

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,46 @@
+const AppError = require('../utlils/app-error');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    messsage: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // eslint-disable-next-line no-console
+    console.error('Error', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
+  }
 };
 
 module.exports = globalErrorHandler;
