8fb04624c799fa21b60ed442a804b7c9f0bebb6c
sheheem <sheheemzainudeen@gmail.com>
2023-12-01 00:40:50 +0530

    bcrypt comparing done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../models/user.model');
 const catchAsync = require('../utlils/catchAsync');
+const AppError = require('../utlils/app-error');
+
+const jwtSign = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '90d',
+  });
 
 exports.signUp = catchAsync(async (req, res) => {
   const newUser = await User.create({
@@ -10,14 +17,31 @@ exports.signUp = catchAsync(async (req, res) => {
     password: req.body.password,
     confirmPassword: req.body.confirmPassword,
   });
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: '90d',
-  });
+  const token = jwtSign(newUser._id);
   res.status(200).json({
     status: 'Success',
     token,
     data: {
       newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new AppError('Email or password is missing', 400));
+  }
+
+  const user = await User.findOne({ email }).select(+password);
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return next(new AppError('Unauthorized', 401));
+  }
+
+  const token = jwtSign(user._id);
+  res.status(200).json({
+    status: 'Success',
+    token,
   });
 });
