a6609c86d3cee0880625e848caa59e5a0a856412
sheheem <sheheemzainudeen@gmail.com>
2023-11-30 22:54:52 +0530

    jwt added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
 const catchAsync = require('../utlils/catchAsync');
 
 exports.signUp = catchAsync(async (req, res) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: '90d',
+  });
   res.status(200).json({
     status: 'Success',
+    token,
     data: {
       newUser,
     },
